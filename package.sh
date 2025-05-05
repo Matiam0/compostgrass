92f26835fd58a5cd9bb9533a6cf74e92bf204aef
Matiam0 <Matiamo+git@proton.me>
2025-05-05 16:09:48 -0700

    Fixed package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 echo "gameVersion"
 read -ps gameversion
 echo "zipping up."
-zip -9 -r ../compostgrass-v$gameversion-dev.1.zip full/*.* /home/matiamo/Git/compostgrass/full/assets/compostgrass/recipes/barrel/compostgrass.json
-zip -9 -r ../compostgrass-v$gameversion-dev.2.zip half/*.* /home/matiamo/Git/compostgrass/half/assets/compostgrass/recipes/barrel/compostgrass.json
+zip -9 -r ../compostgrass-v$gameversion-dev.1.zip full/modinfo.json /home/matiamo/Git/compostgrass/full/assets/compostgrass/recipes/barrel/compostgrass.json
+zip -9 -r ../compostgrass-v$gameversion-dev.2.zip half/modinfo.json /home/matiamo/Git/compostgrass/half/assets/compostgrass/recipes/barrel/compostgrass.json
 echo "complete"
