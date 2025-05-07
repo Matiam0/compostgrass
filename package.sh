feed6d651e71e7d6c49971f48d51948300a59474
Matiam0 <Matiamo+git@proton.me>
2025-05-06 22:55:20 -0700

    Updated package.sh to use version file, added package.ps1 for windows users.

--- package.sh
+++ package.sh
@@ -1,9 +1,6 @@
-echo "gameVersion"
-read -ps gameversion
-echo "zipping up."
-
+gameversion = grep version
 cd full
-zip -9 -r ../../compostgrass-v$gameversion-dev.1.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
+zip -9 -r ../compostgrass-v$gameversion-dev.1.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
 cd ../half
-zip -9 -r ../../compostgrass-v$gameversion-dev.2.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
+zip -9 -r ../compostgrass-v$gameversion-dev.2.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
 echo "complete"
