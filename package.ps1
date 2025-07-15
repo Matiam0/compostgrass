fee72e544efbd151d0d96b0658322b85005bbd45
Matiamo <matiamo+git@proton.me>
2025-07-15 11:37:03 -0700

    Delete package.ps1
    Moving to codeberg

--- package.ps1
+++
@@ -1,6 +0,0 @@
-$version = Get-Content -Path "version"
-Set-Location -Path "full"
-Compress-Archive -Path "*" -DestinationPath "..\compostgrass-v$version-dev.1.zip"
-Set-Location -Path "..\half"
-Compress-Archive -Path "*" -DestinationPath "..\compostgrass-v$version-dev.2.zip"
-Set-Location "..\"
