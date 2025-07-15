bebcef4f99cffdf4b7393a6dd0bd77da475d6168
Matiamo <matiamo+git@proton.me>
2025-07-15 11:37:15 -0700

    Delete package.sh
    Moving to codeberg

--- package.sh
+++
@@ -1,6 +0,0 @@
-gameversion = grep version
-cd full
-zip -9 -r ../compostgrass-v$gameversion-dev.1.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
-cd ../half
-zip -9 -r ../compostgrass-v$gameversion-dev.2.zip modinfo.json assets/compostgrass/recipes/barrel/compostgrass.json
-echo "complete"
